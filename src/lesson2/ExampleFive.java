a1c6cf3a8e89e2c95dd5bafaad92e3ce3b183649
Kevin DesLauriers <kdeslauriers@bayviewglen.ca>
2023-09-19 10:58:52 -0400

    Initial Commit

---
+++ src/lesson2/ExampleFive.java
@@ -0,0 +1,8 @@
+package lesson2;
+
+public class ExampleFive {
+    public static void main(String[] args) {
+        System.out.println("🌳🌳🌳🌳🟫🏃🌳🌳🌳");
+        System.out.println("🌳🌳🌳🌳🟫🌳🌳🌳🌳");
+    }
+}
---
+++ src/lesson2/ExampleTwo.java
@@ -0,0 +1,12 @@
+package lesson2;
+
+public class ExampleTwo {
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
+    }
+}
+
+// Each println went to its own line. Why?
+
